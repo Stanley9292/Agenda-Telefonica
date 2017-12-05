b4b2593eb8416607f83cc220a9c11d9e3044eda8
Stanley9292 <stanalexandrugeorge@gmail.com>
2017-12-05 23:46:40 +0200

    Add files via upload
    Adaugat package-ul InteractiuneGUI

--- src/agenda/telefonica/CarteDeTelefon.java
+++ src/agenda/telefonica/CarteDeTelefon.java
@@ -5,23 +5,45 @@
  */
 package agenda.telefonica;
 
+import InteractiuneGUI.ActionListenerPersonal;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Stan
  */
-public class CarteDeTelefon {
+public class CarteDeTelefon extends JFrame {
+    
+    private Connection conn; //variabila pentru conectarea la baza de date
+    
+    private DefaultTableModel model;
+    private JTable tabel = new JTable();
+    private int randSelectat = 0;
+    
+    private JTextField numeText = new JTextField(15);
+    private JTextField prenumeText = new JTextField(15);
+    private JTextField cnpText = new JTextField(15);
+    private JTextField telefonText = new JTextField(15);
+    private JTextField cautareText = new JTextField(15);
+    
+    private final ActionListenerPersonal actionListenerPersonal;
     
     private static final int lungimeNumar = 10;
     private static final String formatMobil = "07";
     private static final String formatFix = "02";
-
-//  pentru validare 10 cifre  -   \d{10}
-//  
+    
+    protected CarteDeTelefon(Connection conn){
+        this.setConn(conn);
+        actionListenerPersonal = new ActionListenerPersonal(this);
+    }
 
     //getter NrTelefon, verificare daca e null, lungime diferita decat 10 si format nr telefon
     public NrTel getNrTel(String numar) throws Exception{
@@ -35,6 +57,10 @@ public class CarteDeTelefon {
         }else{
             throw new Exception("Formatul introdus este unul gresit!");
         }
+    }
+
+    private void setConn(Connection conn) {
+        this.conn = conn;
     }
     
 }
